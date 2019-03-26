ab74279a66aeb4ad696fd1001711606a4c02dd68
PascalDutlyCsBe <43207179+PascalDutlyCsBe@users.noreply.github.com>
2019-03-26 17:25:18 +0100

    new TEST

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.get('/home', function (req, res) {
+   res.send('Home');
+});
+
+app.listen(process.env.PORT || 80, function () {
+    console.log('Example app listening on port ' + process.env.PORT);
 });
