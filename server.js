9432f7a72ae4038825f98eff7c1f49b847483341
PascalDutlyCsBe <43207179+PascalDutlyCsBe@users.noreply.github.com>
2019-03-26 17:52:37 +0100

    mario.png added

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
 var app = express();
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
@@ -7,6 +12,27 @@ app.get('/', function (req, res) {
 
 app.get('/home', function (req, res) {
    res.send('Home');
+});
+
+app.post('/profile', upload.single('avatar'), function (req, res, next) {
+    // req.file is the `avatar` file
+    // req.body will hold the text fields, if there were any
+});
+
+app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
+    // req.files is array of `photos` files
+    // req.body will contain the text fields, if there were any
+});
+
+var cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
+app.post('/cool-profile', cpUpload, function (req, res, next) {
+    // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
+    //
+    // e.g.
+    //  req.files['avatar'][0] -> File
+    //  req.files['gallery'] -> Array
+    //
+    // req.body will contain the text fields, if there were any
 });
 
 app.listen(process.env.PORT || 80, function () {
