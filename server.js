c0ae572eb83008e69024ea77d39a5265235fc13f
PascalDutlyCsBe <43207179+PascalDutlyCsBe@users.noreply.github.com>
2019-03-27 09:26:31 +0100

    resize works kinda

--- server.js
+++ server.js
@@ -13,13 +13,11 @@ app.post('/profile', upload.single('avatar'), function (req, res, next) {
     // req.body will hold the text fields, if there were any
 });
 
-app.get('/resize', function (req, res){
-gm('/public/mario.png')
+gm('./public/mario.png')
     .resize(40, 40)
     .noProfile()
-    .write('/resize/resize.png', function (err) {
+    .write('./resize', function (err) {
         if (!err) console.log('done');
-    });
 });
 
 app.get('/', function (req, res) {
@@ -35,20 +33,14 @@ app.post('/profile', upload.single('avatar'), function (req, res, next) {
     // req.body will hold the text fields, if there were any
 });
 
-app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
+/*app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
     // req.files is array of `photos` files
     // req.body will contain the text fields, if there were any
-});
+});*/
 
 var cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }]);
 app.post('/cool-profile', cpUpload, function (req, res, next) {
-    // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
-    //
-    // e.g.
-    //  req.files['avatar'][0] -> File
-    //  req.files['gallery'] -> Array
-    //
-    // req.body will contain the text fields, if there were any
+
 });
 
 app.listen(process.env.PORT || 80, function () {
