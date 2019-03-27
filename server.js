8bccc91d0924f0896acc3c6e83845e8e76449c35
PascalDutlyCsBe <43207179+PascalDutlyCsBe@users.noreply.github.com>
2019-03-27 15:34:08 +0100

    upload + resize works

--- server.js
+++ server.js
@@ -1,31 +1,43 @@
+// For the file upload I used this Tutorial: https://www.youtube.com/watch?v=9Qzmri1WaaE
+
 var express = require('express');
 var multer  = require('multer');
-var upload = multer({ dest: 'uploads/' });
+const ejs = require('ejs');
 var fs = require('fs')
     , gm = require('gm').subClass({imageMagick: true});
+const path = require('path');
 
-var app = express();
-
-app.use(express.static('public'));
-
-app.post('/profile', upload.single('avatar'), function (req, res, next) {
-    // req.file is the `avatar` file
-    // req.body will hold the text fields, if there were any
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: function(req, file, cb){
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
 });
 
-    /*gm('./public/*')
-        .resize(40, 40)
-        .noProfile()
-        .write('./resize/sm_.png', function (err) {
-            if (!err) console.log('done');
-        });*/
+var upload = multer({
+    storage: storage
+});
+
+var app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.post('/upload', upload.single('myImage'), function(req, res, next) {
+    const file = req.file;
+    if (!file) {
+        res.render('index');
+    }else {
+        console.log(req.file);
+        res.send(file);
+    }
+});
 
 app.get('/', function (req, res) {
-    res.send(200);
+    res.render('index');
 });
 
 app.get('/resize', function (req, res) {
-   res.send('Resize');
+    res.send('Resize');
     gm('./public/*')
         .resize(40, 40)
         .noProfile()
@@ -46,21 +58,25 @@ app.get('/resize', function (req, res) {
         });
 });
 
-app.post('/profile', upload.single('avatar'), function (req, res, next) {
-    // req.file is the `avatar` file
-    // req.body will hold the text fields, if there were any
+app.get('/api/file', function (req, res) {
+    res.send(200);
+    gm('./public/uploads/*')
+        .resize(720, 720)
+        .write('./public/small/small_image.jpg', function (err) {
+            if (!err) console.log('small resized');
+        });
+    gm('./public/uploads/*')
+        .resize(1280, 1280)
+        .write('./public/medium/medium_image.jpg', function (err) {
+            if (!err) console.log('medium resized');
+        });
+    gm('./public/uploads/*')
+        .resize(1920, 1920)
+        .write('./public/large/large_image.jpg', function (err) {
+            if (!err) console.log('large resized');
+        });
 });
 
-/*app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
-    // req.files is array of `photos` files
-    // req.body will contain the text fields, if there were any
-});*/
-
-var cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }]);
-app.post('/cool-profile', cpUpload, function (req, res, next) {
-
-});
-
-app.listen(process.env.PORT || 80, function () {
-    console.log('Example app listening on port ' + process.env.PORT);
+app.listen(process.env.PORT || 3000, function () {
+    console.log('App listening on port ' + process.env.PORT);
 });
